27f01cbc2f701eea16ec8dbf196b5c25205f0316
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-14 13:41:12 -0400

    Mise en place container list

---
+++ src/components/rate-exchange-list-item.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class RateExchangeListItem extends Component {
+
+    render() {
+        return (
+            <tr>
+                <td>
+                    Test
+                </td>
+                <td>
+                    Graphique
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default RateExchangeListItem;
---
+++ src/containers/rate-exchange-list.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import RateExchangeListItem from "../components/rate-exchange-list-item";
+
+class RateExchangeList extends Component {
+
+    render() {
+        return (
+          <table className="table">
+              <thead>
+                <tr>
+                    <th>Pays</th>
+                    <th className="col-md-6">Valeur de la monnaie par rapport au $</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                    this.props.rateExchangeList.map((rel, index) =>
+                        <RateExchangeListItem key={rel.code+index} rateExchange={rel} />
+                    )
+                }
+              </tbody>
+          </table>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return  {
+        rateExchangeList: state.rateExchangeReducer.rateExchangeList
+    };
+}
+
+export default connect(mapStateToProps, undefined) (RateExchangeList);
