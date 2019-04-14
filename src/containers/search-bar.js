f16a0ce1a94db670451645aae76eb54244b662a0
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-14 13:18:32 -0400

    Recuperation des currencies

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {fetchCountries} from "../actions";
+import {fetchCountries, fetchRateExchange} from "../actions";
 import {connect} from "react-redux";
+const lodash = require("lodash");
 
 class SearchBar extends Component {
 
@@ -10,7 +11,7 @@ class SearchBar extends Component {
 
     renderSelectCountries() {
         return (
-          <select className="form-control search_bar">
+          <select className="form-control search_bar" onChange={e => this.onChangeCountry(e)}>
               {
                   this.props.countries.map(country => (
                           <option key={country.code} value={country.code}>
@@ -30,16 +31,22 @@ class SearchBar extends Component {
         );
     }
 
+    onChangeCountry = event => {
+        const countryCode = event.target.value;
+        const country = lodash.find(this.props.countries, {code: countryCode});
+        this.props.fetchRateExchange(country);
+    };
 }
 
 const mapStateToProps = (store) => {
     return {
         countries: store.countryReducer.countries
     }
-}
+};
 
 const mapDispatchToProps = {
-    fetchCountries
-}
+    fetchCountries,
+    fetchRateExchange
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
